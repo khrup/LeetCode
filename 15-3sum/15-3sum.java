3d52142613b0bdd656b9978def6c835124119ce1
Kong <38851744+khrup@users.noreply.github.com>
2022-04-01 23:59:35 +0900

    Time: 828 ms (12.67%), Space: 119 MB (20.93%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -2,10 +2,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-         List<List<Integer>> answers = new ArrayList<>();
+        public List<List<Integer>> threeSum(int[] nums) {
 
-        Set<String> dupCheck = new HashSet<>();
+        Set<List<Integer>> answers = new HashSet<>();
 
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
@@ -13,10 +12,8 @@ class Solution {
             int x = i + 1;
             while (x < y) {
 
-                String sumString = nums[i] + "" + nums[x] + "" + nums[y] + "";
                 int sum = nums[i] + nums[x] + nums[y];
-                if (!dupCheck.contains(sumString) && sum == 0) {
-                    dupCheck.add(sumString);
+                if (sum == 0) {
                     answers.add(Arrays.asList(new Integer[]{nums[i], nums[x], nums[y]}));
                     x++;
                     y--;
@@ -31,6 +28,6 @@ class Solution {
             }
         }
 
-        return answers;
+        return new ArrayList<>(answers);
     }
 }
